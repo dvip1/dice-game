57c835b4a891f4eebd0a2aaf5c88f2ea184e564b
Dvip <98635463+dvip1@users.noreply.github.com>
2022-11-11 21:25:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+//make a function to generate random numbers between 1 and 6
+function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+function rollDice2() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+function one_to_six(i){
+   if(i==1)return "one";
+    else if(i==2)return "two";
+    else if(i==3)return "three";
+    else if(i==4)return "four";
+    else if(i==5)return "five";
+    else if(i==6)return "six";
+}
+function dicegame(){
+    const temp=rollDice();
+    const temp1=rollDice2();
+    document.querySelector("strong").setAttribute("class", "fa-solid fa-dice-"+one_to_six(temp));
+    document.querySelector("i").setAttribute("class", "fa-solid fa-dice-"+one_to_six(temp1));
+    if  (temp<temp1){
+        document.querySelector("h1").innerHTML = "Player 1 wins";
+    }
+    else if(temp>temp1){
+        document.querySelector("h1").innerHTML = "Player 2 wins";
+        }
+    else{
+        document.querySelector("h1").innerHTML = "Draw";
+    }
+    }
+//make a function to get random value out of an array
+function randomValueFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+var array=['one','two','three','four','five','six'];
